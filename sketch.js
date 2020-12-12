592f1f4f6721602adc61118e688a5e991d4ac0c0
YOYOYJ <72976142+YOYOYJ@users.noreply.github.com>
2020-12-13 05:02:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,9 @@
-
-
-
-
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Constraint = Matter.Constraint;
-var bullet,wall,speed,weight;
+var car,wall,speed,weight;
 
 
 function preload()
@@ -22,9 +17,9 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-	speed=random(550,900);
-	weight=random(20,30);
-	bullet=createSprite(50,200,50,50);
+	speed=random(55,90);
+	weight=random(400,1500);
+	car=createSprite(50,200,50,50);
     wall=createSprite(1500,600,20,hieght/2)
 
 	Engine.run(engine);
@@ -35,20 +30,20 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  bullet.velocityX=speed;
+  car.velocityX=speed;
   //car.collide(wall);
 
-  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
-	  bullet.velocityX=0;
-	  var damage=0.5 * weight * speed * speed/2509;
-	  if (damage>180){
-		  bullet.shapeColor("green");
+  if(wall.x-car.x<(car.width+wall.width)/2){
+	  car.velocityX=0;
+	  var deformation=0.5 * weight * speed * speed/2509;
+	  if (deformation>180){
+		  car.shapeColor("green");
 	  }
-	  if (damage<180 && damage>100){
-		  bullet.shapeColor("yellow");
+	  if (deformation<180 && deformation>100){
+		  car.shapeColor("yellow");
 	  }
-	  if (damage>180){
-		  bullet.shapeColor("red")
+	  if (deformation>180){
+		  car.shapeColor("red")
 	  }
   }
   
@@ -56,6 +51,3 @@ function draw() {
 
  
 }
-
-
-
